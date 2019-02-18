46558b65963d645d96943d954f1ee1245e281dfd
jonhyrsc <joao42a@hotmail.com>
2019-02-18 16:09:48 +0000

    18 fev

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def get_message():
     sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
     # return selected item to the user
     return text
-    return random.choice(sample_responses)
+    #return random.choice(sample_responses)
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
