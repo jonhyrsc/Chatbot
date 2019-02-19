1fab736d10dbd47ebc779edb927e0fad856c1d2b
jonhyrsc <joao42a@hotmail.com>
2019-02-19 15:03:24 +0000

    19fev

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import nltk
 from nltk.tokenize import word_tokenize
 app = Flask(__name__)
 
-nltk.download('punkt')
+
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot (ACCESS_TOKEN)
@@ -35,7 +35,7 @@ def receive_message():
                     text_to_send = message['message']['text']
                     text_tokenized = word_tokenize(text_to_send)
 
-                    send_message(recipient_id, text_to_send)
+                    send_message(recipient_id, text_tokenized)
                     #resposta = tokenize_sentence(text)
                     #response_sent_text = get_message(text_to_send)
                     #response_tokenized = tokenize_sentence(text_to_send)
