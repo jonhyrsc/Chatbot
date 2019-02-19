7f06d72b30ca8633e8b5d0d1f4c9db53af4667b6
jonhyrsc <joao42a@hotmail.com>
2019-02-19 15:35:43 +0000

    19fev

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import random
 import os
 from flask import Flask, request
 from pymessenger.bot import Bot
-import nltk
+import nltk.data
 from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+
 app = Flask(__name__)
 
 
@@ -35,7 +38,7 @@ def receive_message():
                     text_to_send = message['message']['text']
                     text_tokenized = word_tokenize(str(text_to_send))
 
-                    send_message(recipient_id, text_tokenized)
+                    send_message(recipient_id, text_tokenized[1])
                     #resposta = tokenize_sentence(text)
                     #response_sent_text = get_message(text_to_send)
                     #response_tokenized = tokenize_sentence(text_to_send)
