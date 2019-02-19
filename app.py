f499e22054e1a5392df3682e35c1cce4f98b7f58
jonhyrsc <joao42a@hotmail.com>
2019-02-19 15:07:46 +0000

    19fev

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def receive_message():
                     text_to_send = message['message']['text']
                     text_tokenized = word_tokenize(text_to_send)
 
-                    send_message(recipient_id, text_tokenized)
+                    send_message(recipient_id, text_to_send)
                     #resposta = tokenize_sentence(text)
                     #response_sent_text = get_message(text_to_send)
                     #response_tokenized = tokenize_sentence(text_to_send)
